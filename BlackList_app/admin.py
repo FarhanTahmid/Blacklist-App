10974324c498fe8945717375cbf7dbea79e6f313
zaed <zaed@gmail.com>
2022-08-19 10:40:37 +0600

    Added superuser and compaints model

--- BlackList_app/admin.py
+++ BlackList_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from.models import Complain, Users
+
+admin.site.register(Users)
+admin.site.register(Complain)
--- BlackList_app/models.py
+++ BlackList_app/models.py
@@ -7,3 +7,13 @@ class Users(models.Model):
     
     def __str__(self):
         return self.userid
+
+class Complain(models.Model):
+    bully_id = models.CharField(max_length=40, null=False, blank=False, verbose_name="Bully ID",primary_key=True)
+    abuseDescription=models.CharField(max_length=1000,null=False,blank=False,verbose_name="Complain Description")
+    links=models.URLField(max_length=200,null=False,blank=False,verbose_name="Links")
+    anonymity=models.CharField(max_length=20,null=False,blank=False,verbose_name="Anonymity")
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.bully_id
